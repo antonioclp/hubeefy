f4418717db5989022020292ec0a903c538fb8543
antonioclp <antonionetosg6@gmail.com>
2024-10-30 19:22:15 -0300

    add pr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+/**
+ * Development branch.
+ * Homepage.
+ * @returns {JSX.Element}
+ */
+export default function Home(): JSX.Element {
   return (
     <main>
       <section>
